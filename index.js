9f723b5837ac1d4e62a35d71a5841091c7f394f8
zeroxcool <zeroxcool@live.ca>
2018-09-05 19:44:37 -0600

    port change

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/app'));
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 443);
 
-console.log('Application Running on Port 3000');
+console.log('Application Running on Port 443');
